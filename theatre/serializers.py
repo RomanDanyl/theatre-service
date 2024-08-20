034289fe05b4c973a3501979165ddd5b687f6f5c
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-08-20 18:50:47 +0300

    Add serializers, viewsets, endpoints for Actor and Genres models

---
+++ theatre/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from theatre.models import Actor, Genre
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+        )
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
---
+++ theatre/urls.py
@@ -0,0 +1,14 @@
+from rest_framework import routers
+from django.urls import path, include
+
+from theatre.views import GenreViewSet, ActorViewSet
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "theatre"
